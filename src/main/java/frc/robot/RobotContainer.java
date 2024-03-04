91c9f9e182edc7ba5b33fa6f1d55ed7a5a60a86a
Robert Snell <snellrobert06@gmail.com>
2024-03-03 20:57:46 -0500

    update test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,15 +5,15 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.TestCommand;
+import frc.robot.subsystems.Subsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Subsystem m_exampleSubsystem = new Subsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -26,6 +26,6 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    m_driverController.b().whileTrue(new ExampleCommand(m_exampleSubsystem));
+    m_driverController.b().whileTrue(new TestCommand(m_exampleSubsystem));
   }
 }
